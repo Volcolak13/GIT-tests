5bdfd6c06f1bb9e37c897720c26875d5614e2afb
Greg <Volcolak@gmail.com>
2022-05-26 22:18:02 +0300

    Function for CODEWARS task

---
+++ descending_order.py
@@ -0,0 +1,19 @@
+def descending_order(num):
+# Function to take any non-negative integer as an argument and return it with its digits in descending order.
+    newnum = ""
+    list = []
+    list = [int(a) for a in str(num)]
+    while list:
+        max_value = max(list)
+        max_index = list.index(max_value)
+        newnum = newnum + str(max_value)
+#        print(max_value, max_index)
+        list.pop(max_index)
+    return int(newnum)
+
+num = -1
+while num < 0:
+    num = int(input("\nInput non-negative integer number: "))
+if num >= 0 and num == int(num):
+    newnum = descending_order(num)
+    print ("New descending number is: ", newnum, "\n\n")
